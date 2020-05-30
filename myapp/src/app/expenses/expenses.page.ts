f48b19009bee4e1525ca593cc7d959267f0c35fd
AvramAlexandraIoana <37377341+AvramAlexandraIoana@users.noreply.github.com>
2020-05-30 09:07:40 +0100

    added chart in expenses page

--- myapp/src/app/expenses/expenses.page.ts
+++ myapp/src/app/expenses/expenses.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
+import CanvasJS from 'src/app/canvasjs.min.js'
 
 @Component({
   selector: 'app-expenses',
@@ -11,7 +12,38 @@ export class ExpensesPage implements OnInit {
   constructor(private navCtrl: NavController) { }
 
   ngOnInit() {
+    let dataPoints = [];
+	  let dpsLength = 0;
+    let chart = new CanvasJS.Chart("chartContainer", {
+      theme: "light2",
+      animationEnabled: true,
+      title:{
+        text: "Monthly Expense"
+      },
+      data: [{
+        type: "pie",
+        showInLegend: true,
+        toolTipContent: "<b>{name}</b>: ${y} (#percent%)",
+        content: "IOANA",
+        indexLabel: "{name} - #percent%",
+        dataPoints: dataPoints,
+          // { y: 450, name: "Food" },
+          // { y: 120, name: "Insurance" },
+            // { y: 200, name: "Traveling"},
+            // { y: 800, name: "Housing" },
+          // { y: 150, name: "Education" },
+          // { y: 150, name: "Shopping"},
+          // { y: 250, name: "Others" }
+      }]
+    });
+    dataPoints.push({y : 200, name: "Traveling"});
+    dataPoints.push({y : 800, name: "Housing"});
+
+      
+    chart.render();
   }
+
+ 
 
   logout()
   {
